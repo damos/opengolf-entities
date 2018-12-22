5b88cb7abe7dd30d451633060db7f738e9da831c
Darin Amos <darinamos@Darins-MacBook-Pro.local>
2018-12-22 14:06:41 -0500

    Added distance

--- src/main/java/ca/dait/opengolf/entities/course/Course.java
+++ src/main/java/ca/dait/opengolf/entities/course/Course.java
@@ -2,6 +2,7 @@ package ca.dait.opengolf.entities.course;
 
 public class Course {
     private String id;
+    private Double distance;
     private CourseDetails details;
 
     public Course(String id){
@@ -13,11 +14,22 @@ public class Course {
         this.details = details;
     }
 
+    public Course(String id, Double distance, CourseDetails details) {
+        this.id = id;
+        this.distance = distance;
+        this.details = details;
+    }
+
     public String getId(){
         return this.id;
+    }
+
+    public Double getDistance(){
+        return this.distance;
     }
 
     public CourseDetails getDetails(){
         return this.details;
     }
+
 }
