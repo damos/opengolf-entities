91b353add6949f276a65ad52713a3efae83df856
Darin Amos <darinamos@Darins-MacBook-Pro.local>
2019-01-27 14:07:56 -0500

    re-write

--- src/main/java/ca/dait/opengolf/entities/course/Course.java
+++ src/main/java/ca/dait/opengolf/entities/course/Course.java
@@ -1,35 +1,108 @@
 package ca.dait.opengolf.entities.course;
 
 public class Course {
-    private String id;
+    private String remoteId;
     private Double distance;
-    private CourseDetails details;
+    private String facilityName;
+    private String nickName;
+    private String municipality;
+    private String state;
+    private String country;
+    private String googlePlaceId;
+    private Hole holes[];
 
-    public Course(String id){
-        this.id = id;
+    public Course(){}
+    public Course(String remoteId){
+        this.remoteId = remoteId;
     }
 
-    public Course(String id, CourseDetails details) {
-        this.id = id;
-        this.details = details;
+    public String getRemoteId() {
+        return remoteId;
     }
 
-    public Course(String id, Double distance, CourseDetails details) {
-        this.id = id;
+    public void setRemoteId(String remoteId) {
+        this.remoteId = remoteId;
+    }
+
+    public Double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(Double distance) {
         this.distance = distance;
-        this.details = details;
     }
 
-    public String getId(){
-        return this.id;
+    public String getFacilityName() {
+        return facilityName;
     }
 
-    public Double getDistance(){
-        return this.distance;
+    public void setFacilityName(String facilityName) {
+        this.facilityName = facilityName;
     }
 
-    public CourseDetails getDetails(){
-        return this.details;
+    public String getNickName() {
+        return nickName;
     }
 
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getMunicipality() {
+        return municipality;
+    }
+
+    public void setMunicipality(String municipality) {
+        this.municipality = municipality;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getFullName(){
+        return (this.nickName == null) ? this.facilityName : this.facilityName + ": " + this.nickName;
+    }
+
+    public String getGooglePlaceId(){
+        return this.googlePlaceId;
+    }
+    public void setGooglePlaceId(String googlePlaceId){
+        this.googlePlaceId = googlePlaceId;
+    }
+    public Hole[] getHoles(){
+        return this.holes;
+    }
+    public void setHoles(Hole holes[]){
+        this.holes = holes;
+    }
+
+    public static class Hole{
+        private double lat;
+        private double lon;
+
+        public Hole(double lat, double lon){
+            this.lat = lat;
+            this.lon = lon;
+        }
+        public double getLat(){
+            return this.lat;
+        }
+
+        public double getLon(){
+            return this.lon;
+        }
+    }
 }
